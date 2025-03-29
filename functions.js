827e1cb7fde577f36adb6e7668f974854a5fa589
Nomaan <nomaan.gagan@gmail.com>
2025-03-30 02:26:58 +0530

    new functions.js

---
+++ functions.js
@@ -0,0 +1,19 @@
+// //functions help reduce redundancy
+// // making of function
+// function myFunction() {
+// console.log("hello");
+// console.log("hello");
+// }
+
+// myFunction(); //function call
+
+// function mFunction(msg){
+//     console.log(msg);
+// }
+// mFunction("heyyyggsss");
+
+
+function vowels(str){
+    str = 
+    
+}
